51d4d9f75f752d83a6707bd3704f806ab4f0e67a
DESKTOP-RL0KI24\NY <yagnyatinskiy@bk.ru>
2022-04-07 21:56:12 +0400

    go to go

---
+++ Assets/LEGO/Scripts/Gameplay/GroundHazard.cs
@@ -0,0 +1,19 @@
+﻿using Unity.LEGO.Game;
+using UnityEngine;
+
+namespace Unity.LEGO.Gameplay
+{
+    public class GroundHazard : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other) 
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                GameOverEvent evt = Events.GameOverEvent;
+                evt.Win = false;
+                EventManager.Broadcast(evt);
+            }
+        }
+    }
+}
+
